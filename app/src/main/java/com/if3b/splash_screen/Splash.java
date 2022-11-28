4cd0ffd79a05494e3422ac1dbd4c7892032c6e9b
rizviroshan <rizviroshan10@mhs.mdp.ac.id>
2022-11-28 11:45:12 +0700

    new

---
+++ app/src/main/java/com/if3b/splash_screen/Splash.java
@@ -0,0 +1,28 @@
+package com.if3b.splash_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler HDL = new Handler();
+        HDL.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        }, 2000);
+    }
+}
